de3b1832ee028e93a5c47f1cec443f44ff80d4dd
Yury Vakolov <yuryvakolov@yandex.ru>
2023-01-22 16:57:14 +0300

    created form builder initial structure

--- libs/common-ui/src/lib/form-builder/form-builder.component.ts
+++ libs/common-ui/src/lib/form-builder/form-builder.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {IdGeneratorService} from "apps/rosbank/src/app/core/id-generator.service";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'front-web-form-builder',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-builder.component.scss'],
 })
 export class FormBuilderComponent {
+  public form = this.fb.group([]);
+
+  @Input() id = this.idGeneratorService.generate();
+
+  constructor(private idGeneratorService: IdGeneratorService, private fb: FormBuilder) {
+  }
+
 
 }
